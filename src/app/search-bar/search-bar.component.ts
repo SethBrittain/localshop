62227836ed383da6ca285362eb8a79f93abb2946
Seth <sethabrittain@gmail.com>
2022-11-12 10:35:15 -0600

    dropdown fix

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -12,4 +12,10 @@ export class SearchBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleCategoryDropdown(): void {
+    let dropdowns = document.querySelectorAll('.dropdown-menu');
+    for (let i=0; i<dropdowns.length; i++) { if (dropdowns[i].id != "category-dropdown") dropdowns[i].classList.remove('show'); }
+    let dropdown : HTMLButtonElement | null = document.querySelector('#category-dropdown');
+    dropdown?.classList.toggle('show');
+  }
 }
