417bcb7b18bcc1fd774fd099aeb2077b0587c78c
Seth Brittain <sethabrittain@gmail.com>
2023-02-22 15:41:01 -0600

    redesign tests

--- Backend/src/main/java/com/localle/backend/model/Product.java
+++ Backend/src/main/java/com/localle/backend/model/Product.java
@@ -1,83 +1,32 @@
 package com.localle.backend.model;
 
-import java.math.BigDecimal;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "products")
-public class Product {
-
-    private Long id;
-    //@Embedded private Store store;
-    private String productName;
-    private BigDecimal productPrice;
-    private String productImagePath;
- 
-    public Product(/*Store store,*/ String productName, BigDecimal productPrice, String productImagePath) {
-        //this.store = store;
-        this.productName = productName;
-        this.productPrice = productPrice;
-        this.productImagePath = productImagePath;
-    }
- 
+public class Product
+{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    /*
-    @JoinColumn(name = "store_id")
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
     @ManyToOne
-    public Store getStore() {
-        return store;
-    }
-    public void setStore(Store store) {
-        this.store = store;
-    }
-    */
+    @JoinColumn(name="store_id", nullable=false)
+    private long storeId;
 
-    @Column(name = "product_name", nullable = false)
-    public String getProductName() {
-        return productName;
-    }
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
- 
-    @Column(name = "product_price", nullable = false)
-    public BigDecimal getProductPrice() {
-        return productPrice;
-    }
-    public void setProductPrice(BigDecimal productPrice) {
-        this.productPrice = productPrice;
-    }
- 
-    @Column(name = "product_image_path", nullable = false)
-    public String getProductImagePath() {
-        return productImagePath;
-    }
-    public void setProductImagePath(String productImagePath) {
-        this.productImagePath = productImagePath;
-    }
 
-    @Override
-    public String toString() {
-        return "Product [" +
-            "id=" + id +
-            //"store=" + store.toString() +
-            "productName=" + productName +
-            "productPrice=" + productPrice +
-            "productImagePath=" + productImagePath +
-       "]";
+    private String productName;
+
+    public Product(){}
+    public Product(String productName, long storeId)
+    {
+        
     }
 }
--- Backend/src/main/java/com/localle/backend/model/Store.java
+++ Backend/src/main/java/com/localle/backend/model/Store.java
@@ -1,56 +1,21 @@
 package com.localle.backend.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.OneToMany;
 
 @Entity
-@Table(name = "stores")
+@Table(name="stores")
 public class Store {
-
-    private Long id;
-    private String storeName;
-    private String storeImagePath;
- 
-    public Store(String storeName, String storeImagePath) {
-        this.storeName = storeName;
-        this.storeImagePath = storeImagePath;
-    }
- 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
 
-    @Column(name = "store_name", nullable = false)
-    public String getStoreName() {
-        return storeName;
-    }
-    public void setStoreName(String storeName) {
-        this.storeName = storeName;
-    }
- 
-    @Column(name = "store_image_path", nullable = false)
-    public String getStoreImagePath() {
-        return storeImagePath;
-    }
-    public void setStoreImagePath(String storeImagePath) {
-        this.storeImagePath = storeImagePath;
-    }
 
-    @Override
-    public String toString() {
-        return "Store [" +
-            "id=" + id +
-            "storeName=" + storeName +
-            "storeImagePath=" + storeImagePath +
-       "]";
-    }
+    private String storeName;
+
+    
+    private String address;
+
+    @OneToMany(mappedBy = "Store")
+    private Set<Product> products;
 }
